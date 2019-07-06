7a0fa60a68ef4be962df95f7e5742df1a0170968
LUNKUS <sigitas@lunkus.com>
2019-07-06 08:51:09 +0300

    omit commented code for close connection

--- src/com/recbill/controller/dal/InvoiceDal.java
+++ src/com/recbill/controller/dal/InvoiceDal.java
@@ -63,21 +63,4 @@ public class InvoiceDal {
 
         return invoices;
     }
-
-//    private void close(Connection conn, Statement stmt, ResultSet rs) {
-//
-//        try {
-//            if(conn != null) {
-//                conn.close();
-//            }
-//            if (stmt != null) {
-//                stmt.close();
-//            }
-//            if (rs != null) {
-//                rs.close(); // doesn't really close those ... just put back to the connections pool
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
